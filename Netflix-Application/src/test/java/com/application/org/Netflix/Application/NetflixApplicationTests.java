093ff6294fd94e2a422e4297b8ed2c3ccaf908c3
saikrishna4343 <saikrishnapvn@gmail.com>
2020-05-19 23:45:01 -0500

    intial commit

---
+++ Netflix-Application/src/test/java/com/application/org/Netflix/Application/NetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.org.Netflix.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
